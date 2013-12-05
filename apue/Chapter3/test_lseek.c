d1aaf4415163afbeec08dd801ea99d3482138aa1
zhuxu <zhuxu107@gmail.com>
2013-12-05 15:36:44 +0800

    test its standard input to see whether it is capable of seeking

---
+++ apue/Chapter3/test_lseek.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_lseek.c
+ *
+ *    Description:  example of chapter3 apue book
+ *
+ *        Version:  1.0
+ *        Created:  2013年12月05日 15时31分16秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ZHU XU (~_~), splk008@qq.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "apue.h"
+
+int main(int argc, char *argv[])
+{
+	if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
+		printf("cannot seek\n");
+	else
+		printf("seek OK\n");
+
+	return 0;
+}
+
