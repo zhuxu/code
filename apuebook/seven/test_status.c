b115c05033f029742a873d28e371b81cec6ed3f3
zhuxu <zhuxu107@gmail.com>
2014-01-13 21:51:36 +0800

    test the exit status

---
+++ apuebook/seven/test_status.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_status.c
+ *
+ *    Description:  exame the exit status
+ *
+ *        Version:  1.0
+ *        Created:  2014年01月13日 21时48分40秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ZHU XU (~_~), splk008@qq.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world!");
+}
